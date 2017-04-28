57740e6e085934c4860d6ba7737b426d06173cbb
Bobby Burden <bobby@brb3.org>
2017-04-27 20:21:40 -0400

    Add Makefile, start client.

--- src/tote.cr
+++ src/tote.cr
@@ -1,13 +1,28 @@
 require "./tote/*"
 require "option_parser"
 
-OptionParser.parse! do |parser|
-  parser.banner = "Usage: tote [arguments] [file]"
-  parser.on("-v", "--version", "Show the version number") {
-    puts Tote::VERSION
-  }
-  parser.on("-h", "--help", "Show this help message") { puts  parser }
+module Tote
+  class Client
+
+    @socket = "" 
+
+    def run
+      OptionParser.parse! do |parser|
+        parser.banner = "Usage: tote [arguments] [file]"
+        parser.on("-v", "--version", "Show the version number") {
+          puts Tote::VERSION
+        }
+        parser.on("-h", "--help", "Show this help message") { puts parser }
+        parser.on(
+          "-s SOCKET", "--server SOCKET", "Use alternate tote server"
+        ) { |arg| @socket = arg }
+      end
+
+      puts @socket
+    end
+
+  end
 end
 
-module Tote
-end
+tote = Tote::Client.new
+tote.run()
